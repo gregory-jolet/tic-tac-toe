96d7cd99946bf43dedb4ed1aaf3c975a4fdd6889
Gregory <jonville.gregory@gmail.com>
2024-03-28 14:33:17 +0100

    Custom Font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        workbench: 'Workbench'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
