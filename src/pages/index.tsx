fffc89090071e9c8d4029d722b2e9ca1b9ef2f0c
Gregory <jonville.gregory@gmail.com>
2024-03-28 15:26:32 +0100

    grid style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,26 @@
-import Image from "next/image";
 
 
 export default function Game() {
-  return <h1>HELLO WORLD</h1>;
+  return (
+    <>
+      <h1 className="text-center">Tic Tac Toe</h1>
+      <div className="max-w-23 mx-auto p-2 d-flex justify-center items-center">
+        <div className="text-center text-2xl tracking-normal">
+          <p>xTurn</p>
+        </div>
+        <div className="grid grid-cols-3 gap-5">
+          {[...Array(9)].map((v, idx) => {
+            return (
+              <div
+
+                key={idx}
+                className="bg-square rounded-lg shadow-square text-center text-6xl leading-loose w-28 h-28 cursor-pointer">
+                X
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
