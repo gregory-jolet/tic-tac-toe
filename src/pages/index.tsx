57ac1f90a3afbc9c879fb8cb8898f860cc9b57e8
Gregory <jonville.gregory@gmail.com>
2024-03-28 19:30:50 +0100

    Lasts features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Head from "next/head";
 import { useState, useEffect } from "react";
+
 
 /* Winning combination */
 const WINNING_COMBO = [
@@ -16,8 +18,12 @@ export default function Game() {
   /* Add X or O */
   const [xTurn, setXTurn] = useState(true);
   /* Winner */
-  const [won, steWon] = useState(false);
-  const [winner, setWinner] = useState("");
+  const [won, setWon] = useState(false);
+  const [wonCombo, setWonCombo] = useState([]);
+  const [winner, setWinner] = useState('');
+
+  /* Modal */
+  const [modalTitle, setModalTitle] = useState('');
 
   /* Draw */
   const [isDraw, setIsDraw] = useState(false);
@@ -58,42 +64,64 @@ export default function Game() {
         boardData[a] === boardData[b] &&
         boardData[a] === boardData[c]
       ) {
-        steWon(true);
-        setWinner(xTurn ? "O" : "X")
+        setWon(true);
+        setWonCombo([a, b, c]);
+        setWinner(xTurn ? "O" : "X");
+        setModalTitle(`Player ${!xTurn ? "X" : "O"} won !`);
       }
     });
   };
 
   const checkDraw = () => {
     let check = Object.keys(boardData).every((v) => boardData[v]);;
-    if (check) setIsDraw(check);
+    if (check) {
+      setIsDraw(check);
+      setModalTitle(`Round draw !`);
+    }
   };
 
   let head;
   if (won) {
-    head = <p>{`Winner: ${winner}`}</p>;
+    head = <p className="mb-2">{`Winner: ${winner}`}</p>;
   } else if (isDraw) {
-    head = <p>{`Games Draw`}</p>;
+    head = <p className="mb-2">{`Games Draw`}</p>;
   }
 
+  const reset = () => {
+    setBoardData({
+      0: "", 1: "", 2: "", 3: "", 4: "", 5: "", 6: "", 7: "", 8: "",
+    });
+    setXTurn(true);
+    setWon(false);
+    setWonCombo([]);
+    setIsDraw(false);
+    setModalTitle("");
+  };
 
   return (
     <>
-      <h1 className="text-center">Tic Tac Toe</h1>
-      <div className="max-w-23 mx-auto p-2 d-flex justify-center items-center">
-        <div className="text-center text-2xl tracking-normal">
-          {!won && <p className="mb-2">Player : {player}</p>}
-          { head }
+      <Head>
+        <title>Tic-Tac-Toe</title>
+      </Head>
+      <div className="h-svh flex flex-col justify-content-center items-center justify-center">
+        <h1 className="text-center">Tic Tac Toe</h1>
+        <div className="max-w-23 mx-auto p-2 d-flex justify-center items-center">
+          <div className="grid grid-cols-3 gap-5">
+            {[...Array(9)].map((value, index) => {
+              return (
+                <div key={index} onClick={() => {
+                  updateBoardData(index)
+                }} className={`${wonCombo.includes(index) ? "highlight" : ""} bg-square rounded-lg shadow-square text-center text-6xl leading-loose w-28 h-28 cursor-pointer`}>
+                  {boardData[index]}
+                </div>);
+            })}
+          </div>
         </div>
-        <div className="grid grid-cols-3 gap-5">
-          {[...Array(9)].map((value, index) => {
-            return (
-              <div key={index} onClick={() => {
-                updateBoardData(index)
-              }} className="bg-square rounded-lg shadow-square text-center text-6xl leading-loose w-28 h-28 cursor-pointer">
-                {boardData[index]}
-              </div>);
-          })}
+        <div className={modalTitle ? "overlay" : ""}>
+          <div className={`${modalTitle ? "show" : ""} modal w-64 rounded-2xl shadow-lg bg-white d-flex flex-col align-items-center p-6 top-1/2`}>
+            <div className="mb-4">{modalTitle}</div>
+            <button className="w-100 h-9 text-5xl" onClick={reset}>New Game</button>
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     fontFamily: {
       workbench: 'Workbench'
     },
     backgroundColor: {
-      'square': '#eee'
+      'square': '#eee',
+      'slate-200':  'rgb(226 232 240)'
+
     },
     boxShadow: {
       'square': '0px 4px #ddd'
