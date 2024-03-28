ce9258077ebca93f471809dbe6af6190c933cd02
Gregory <jonville.gregory@gmail.com>
2024-03-28 15:56:53 +0100

    round of the game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,45 @@
-
+import { useState } from "react";
 
 export default function Game() {
+  /* Add X or O */
+  const [xTurn, setXTurn]: boolean = useState(true);
+  const [boardData, setBoardData] = useState({
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+    8: ""
+  });
+
+  const updateBoardData = (index) => {
+    if (!boardData[index]) {
+      // Check if index of boardData is empty
+      let value = xTurn ? "X" : "O";
+      console.warn('value ', value, ' xTurn ', xTurn)
+      setBoardData({ ...boardData, [index]: value })
+      setXTurn(!xTurn);
+    }
+  }
+
   return (
     <>
       <h1 className="text-center">Tic Tac Toe</h1>
       <div className="max-w-23 mx-auto p-2 d-flex justify-center items-center">
         <div className="text-center text-2xl tracking-normal">
-          <p>xTurn</p>
+          <p className="mb-2">Player : {xTurn ? "X " : "O "}</p>
         </div>
         <div className="grid grid-cols-3 gap-5">
-          {[...Array(9)].map((v, idx) => {
+          {[...Array(9)].map((value, index) => {
             return (
-              <div
-
-                key={idx}
-                className="bg-square rounded-lg shadow-square text-center text-6xl leading-loose w-28 h-28 cursor-pointer">
-                X
-              </div>
-            );
+              <div key={index} onClick={() => {
+                updateBoardData(index)
+              }} className="bg-square rounded-lg shadow-square text-center text-6xl leading-loose w-28 h-28 cursor-pointer">
+                {boardData[index]}
+              </div>);
           })}
         </div>
       </div>
