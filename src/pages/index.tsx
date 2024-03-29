ad8db18853651e64e901541fc51512c4aaaacc0b
Gregory <jonville.gregory@gmail.com>
2024-03-29 11:05:39 +0100

    Points

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,10 @@ export default function Game() {
   const [wonCombo, setWonCombo] = useState([]);
   const [winner, setWinner] = useState('');
 
+  /* Points */
+  let [xPoint, setXPoint] = useState(0);
+  let [oPoint, setOPoint] = useState(0);
+
   /* Modal */
   const [modalTitle, setModalTitle] = useState('');
 
@@ -39,7 +43,6 @@ export default function Game() {
     7: "",
     8: ""
   });
-  const player = xTurn ? "X " : "O ";
 
   useEffect(() => {
     checkWinner();
@@ -67,10 +70,22 @@ export default function Game() {
         setWon(true);
         setWonCombo([a, b, c]);
         setWinner(xTurn ? "O" : "X");
+        winPoint(xTurn ? "O" : "X");
         setModalTitle(`Player ${!xTurn ? "X" : "O"} won !`);
       }
+
     });
   };
+
+  const winPoint = (win) => {
+    if (win === "X") {
+      setXPoint(xPoint + 1);
+      console.log('Le joueur X a gangné !');
+    } else if (win === "O") {
+      setOPoint(oPoint + 1);
+      console.log('Le joueur O a gangné !');
+    }
+  }
 
   const checkDraw = () => {
     let check = Object.keys(boardData).every((v) => boardData[v]);;
@@ -104,28 +119,30 @@ export default function Game() {
         <title>Tic-Tac-Toe</title>
       </Head>
       <h1 className="text-center text-xl mt-10">Tic Tac Toe</h1>
-      <div className="h-svh flex flex-col justify-content-center items-center justify-center">
-        
-        <div className="max-w-23 mx-auto p-2 d-flex justify-center items-center">
-        <p className="text-2xl">Player: {xTurn === true ? "X" : "O"}</p>
-          <div className="grid grid-cols-3 gap-5">
-            {[...Array(9)].map((value, index) => {
-              return (
-                <div key={index} onClick={() => {
-                  updateBoardData(index)
-                }} className={`${wonCombo.includes(index) ? "highlight" : ""} text-square bg-square rounded-lg shadow-square text-center text-6xl leading-loose w-28 h-28 cursor-pointer`}>
-                  {boardData[index]}
-                </div>);
-            })}
-          </div>
-        </div>
-        <div className={modalTitle ? "overlay" : ""}>
-          <div className={`${modalTitle ? "show" : ""} modal w-64 rounded-2xl shadow-lg bg-white d-flex flex-col align-items-center p-6 top-1/2`}>
-            <div className="mb-4">{modalTitle}</div>
-            <button className="w-100 h-9 text-5xl" onClick={reset}>New Game</button>
-          </div>
+      <div className={modalTitle ? "overlay" : ""}>
+        <div className={`${modalTitle ? "show" : ""} modal w-64 rounded-2xl shadow-lg bg-white d-flex flex-col align-items-center p-6 top-1/2`}>
+          <div className="mb-4">{modalTitle}</div>
+          <button className="w-100 h-9 text-5xl" onClick={reset}>New Game</button>
         </div>
       </div>
+      <div className="max-w-23 mx-auto p-2 d-flex justify-center items-center">
+        <p className="text-2xl mb-2">Player: {xTurn === true ? "X" : "O"}</p>
+        <div className="grid grid-cols-3 gap-5">
+          {[...Array(9)].map((value, index) => {
+            return (
+              <div key={index} onClick={() => {
+                updateBoardData(index)
+              }} className={`${wonCombo.includes(index) ? "highlight" : ""} text-square bg-square rounded-lg shadow-square text-center text-6xl leading-loose w-28 h-28 cursor-pointer`}>
+                {boardData[index]}
+              </div>);
+          })}
+        </div>
+      </div>
+      <div className="text-center mt-3">
+        <p className="text-2xl">Player X: {xPoint}</p>
+        <p className="text-2xl">Player O: {oPoint}</p>
+      </div>
+
     </>
   );
 }
